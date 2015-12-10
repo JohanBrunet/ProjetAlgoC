49a97c753910b4fed60b848cc612c9b7f613bb1c
QuentinBouygues <Quentin.bouygues@hotmail.fr>
2015-12-10 12:50:44 +0100

    Début d'implémentation du main

--- Game.h
+++ Game.h
@@ -7,11 +7,13 @@
 
 typedef Game Game;
 
-Game CeateGame(int gridWidth, int gridHeight); // Cée une nouvelle partie avec les dimensions de la grille que l'on veut
-Player getPlayer(int numPlayer); // Renvoie le joueur a partir de son indice dans la liste de joueurs
+Game createGame(int gridWidth, int gridHeight); // Cée une nouvelle partie avec les dimensions de la grille que l'on veut
+Player[] createPlayersList(); // Crée la liste de joueurs, ajoute deux joueurs et crée la liste de bateaux de ces joueurs
+Player getNumPlayer(Player player); // Renvoie le joueur a partir de son indice dans la liste de joueurs
 bool isPlayerActive(int numPlayer); // Renvoie true si c'est le tour du joueur d'indice passé en paramètre, false autrement
-int shoot(Position posiion); // Effectue un tir sur la grille du joueur non actif, renvoie 0 si le tir tombe a l'eau, 1 si on est en vue d'un bateau, et 2 si on touche
-void setNextPlayerAcive(); // Défint l'autre joueur comme étant actif
+int shoot(Position posiion); // Effectue un tir sur la grille du joueur non actif, renvoie 0 si le tir tombe a l'eau, 1 si on est en vue d'un bateau, et 2 si on touche, renvoie -1 en cas de coordonnées invalides
+void setNextPlayerAcive(); // Défint l'autre joueur comme étant actif, si aucun n'est actif défini le premier joueur comme actif
+Player getActivePlayer(); // Renvoie le joueur actif
 
 
 
--- main.c
+++ main.c
@@ -1,8 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include "Boat.h"
+#include "BoatList.h"
+#include "Game.h"
+#include "Grid.h"
+#include "Player.h"
+#include "Position.h"
 
 int main()
 {
-    printf("Hello world!\n");
+    printf("Bienvenue dans la partie\n");   // Signale à l'utilisateur qu'il commence une partie
+
+    Game game = createGame(20,20);  // Création de la partie avec initialisation des joueurs, prend en paramètres la taille e la grille
+    game.setNextPlayerAcive();  // Rend le premier joueur actif
+    Player joueurActif = game.getActivePlayer();    // Récupère le joueur actif
+    printf("Le joueur 1 va placer ses bateaux\n");
+    joueurActif.placeBoats();   // Le joueur actif place ses bateaux
+    game.setNextPlayerActive();     // On passe au joueur suivant
+    printf("Le joueur 2 va placer ses bateaux\n");
+    joueurActif = game.getActivePlayer();   // Le deuxième joueur place ses bateaux
+    joueurActif.placeBoats();   // Le deuxième joueur place ses bateaux
+    game.setNextPlayerActive();     // On passe au joueur suivant
+    joueurActif = game.getActivePlayer();   // On récupère le joueur actif
+
+    while (not joueurActif.isPlayerDead()) {
+        printf("C'est au joueur %d de tirer !\n", game.getNumPlayer(joueurActif));  // Le joueur actif va effectuer un tir
+        printf("Veuillez entrer la coordonnée x du tir");
+        int x;
+        scanf("%d", &x);    // Récupère la coordonnée x saisie par l'utilisateur
+        printf("Veuillez entrer la coordonnée y du tir");
+        int y;
+        scanf("%d", &y);    // Récupère la coordonnée y saisie par l'utilisateur
+        Position position = createPos(x,y);
+        game.shoot(position);   // On tire sur la position désignée par le joueur
+        // Faire le if sur le résultat du tir
+        // Afficher le résultat du tir
+        game.setNextPlayerActive();     // On passe au joueur suivant
+        joueurActif = game.getActivePlayer();   // On récupère le joueur actif
+    }
+    game.setNextPlayerActive();     // Le joueur suivant est le vainqueur car le joueur précédent est mort
+    joueurActif = game.getActivePlayer();   // On récupère le joueur gagnant
+    printf("FIN DE PARTIE, le joueur %d a gagné, bien joué !\n", game.getNumPlayer(joueurActif));   // On affiche qui a gagné la partie
+
+    sleep(5000);
     return 0;
 }
+
+
