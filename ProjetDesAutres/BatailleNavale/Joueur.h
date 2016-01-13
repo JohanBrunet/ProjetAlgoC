0ae3dfc23444ed3435c599fb31e7e6ff577e00b8
QuentinBouygues <quentin.bouygues@hotmail.fr>
2016-01-13 12:19:33 +0100

    Joueur + bug joueur sur bateauxNonCoules

--- ProjetDesAutres/BatailleNavale/Joueur.h
+++ ProjetDesAutres/BatailleNavale/Joueur.h
@@ -12,9 +12,21 @@ typedef struct TJoueur TJoueur {
 typedef TJoueur* Joueur;
 
 /* Créer un joueur vide (sans bateau) avec seulement son numéro  */
-Joueur creerJoueur();
+Joueur creerJoueur(int numJoueur);
 
 /* Renvoie l'ensemble des bateaux restantes du joueur  */
 Bateaux bateauxNonCoules(Joueur j);
+
+/* Renvoie le numéro du joueur */
+int getNumJoueur(Joueur j);
+
+/* Définit le numéro du joueur */
+void setNumJoueur(Joueur j, int numJoueur);
+
+/* Définit les bateaux du joueur */
+void setBateauxJoueur(Joueur j, Bateaux listeBateaux);
+
+/* Renvoie les bateaux du joueur */
+Bateaux getBateauxJoueur(Joueur j);
 
 #endif
