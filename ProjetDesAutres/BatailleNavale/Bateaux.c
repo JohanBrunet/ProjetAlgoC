9a815e65a73c7a7bd61fca5f00f664da24e7e778
JohanBrunet <brunet.johan.23@gmail.com>
2016-01-13 12:53:35 +0100

    Implémentation Bateaux.c

--- ProjetDesAutres/BatailleNavale/Bateaux.c
+++ ProjetDesAutres/BatailleNavale/Bateaux.c
@@ -9,13 +9,24 @@ Bateaux creerBateaux() {
 }
 
 /* Ajoute à la suite un bateau (ensemble de coordonnées) dans l'ensemble des Bateaux */
-Bateaux ajouterBateau(Bateauxbx, Bateau b);
+Bateaux ajouterBateau(Bateaux bx, Bateau b) {
+	//TODO ajouter un bateau b à la liste bx
+	return bx;
+}
 
 /* Supprime un bateau de l'ensemble des Bateaux si celui-ci est vide */
-Bateaux supprimerBateau(Bateaux bx,Bateau b);
+Bateaux supprimerBateau(Bateaux bx, int indice) {
+	bx->bateaux[indice] = NULL;
+	return bx;
+}
 
 /* Renvoie le nombre de bateaux dans l'ensemble Bateaux (la taille de l'ensemble) */
-int nombreBateaux(Bateaux bx);
+int nombreBateaux(Bateaux bx) {
+	return sizeof(bx->bateaux);
+}
 
 /* Cherche dans l'ensemble des Bateaux le bateau donné en paramètre (ses coordonnées) et le renvoie s'il appartient à l'ensemble sinon "ce bateau a été coulé" */
-Bateau recupererBateau(Bateaux bx,Bateau b);
+Bateau recupererBateau(Bateaux bx, int indice) {
+	//TODO récuérer un bateau b dans la liste bx
+	return bx->bateaux[indice];
+}
